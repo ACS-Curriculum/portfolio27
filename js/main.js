2fea905143ba396e85bac5025a16d3d3e03b3ad3
treinartz <thomas.reinartz@gmail.com>
2022-12-24 14:58:21 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import MyTest from "./sketch.js";
+import RockRunner from "./rockPaper/RockRunner.js";
+//import CircleRunner from "./circleCircle/CircleRunner.js";
+
+new RockRunner(400, 400, "blue");
+new MyTest(400, 400, "red");
+//new CircleRunner(400, 400, "green");
---
+++ js/main2.js
@@ -0,0 +1,7 @@
+import CircleRunner from "./circleCircle/CircleRunner.js";
+//import MyTest from "./sketch.js";
+import TileRunner from "./tile.js";
+
+//new MyTest(400, 400, "red");
+new TileRunner(400, 400, "red");
+new CircleRunner(400, 400, "green");
