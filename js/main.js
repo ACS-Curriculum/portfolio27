c01836c4c4d0950d95b7fde160c1ed410cba9734
treinartz <thomas.reinartz@gmail.com>
2022-12-25 10:26:06 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ import MyTest from "./sketch.js";
 import RockRunner from "./rockPaper/RockRunner.js";
 //import CircleRunner from "./circleCircle/CircleRunner.js";
 
-new RockRunner(400, 400, "blue");
-new MyTest(400, 400, "red");
+new RockRunner(400, 400, "gamestwo");
+new MyTest(400, 400, "gamesone");
 //new CircleRunner(400, 400, "green");
